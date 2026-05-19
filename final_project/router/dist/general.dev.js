148e7a33bb1f09500efe37ef3d562467bcf810da
aycman <man.hajilou@gmail.com>
2026-05-19 11:18:26 +0200

    task 11 done

--- final_project/router/dist/general.dev.js
+++ final_project/router/dist/general.dev.js
@@ -36,11 +36,8 @@ public_users.post("/register", function (req, res) {
   return res.status(404).json({
     message: "Unable to register user."
   });
-});
-
-var axios = require('axios'); // Get the book list available in the shop
+}); // Get the book list available in the shop
 //using promise callbacks or async-await + axios
-
 
 public_users.get('/', function (req, res) {
   new Promise(function (resolve, reject) {
@@ -56,24 +53,6 @@ public_users.get('/', function (req, res) {
       message: err
     });
   });
-}); // Get book details based on ISBN
-
-public_users.get('/isbn/:isbn', function (req, res) {
-  //retrieve the isbn from the request parameter
-  var isbn = req.params.isbn; //convert object values to an array and find the matching book
-
-  var bookList = Object.values(books);
-  var foundBook = bookList.find(function (book) {
-    return book.ISBN === isbn;
-  }); //send the book details as response
-
-  if (foundBook) {
-    return res.status(200).json(foundBook);
-  } else {
-    return res.status(401).json({
-      message: "Book not found"
-    });
-  }
 }); // Get book details based on author
 
 public_users.get('/author/:author', function (req, res) {
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,7 +26,6 @@ public_users.post("/register", (req,res) => {
 });
 
 
-const axios = require('axios');
 // Get the book list available in the shop
   //using promise callbacks or async-await + axios
 public_users.get('/', function (req, res) {
@@ -46,23 +45,6 @@ public_users.get('/', function (req, res) {
   }); 
 });
 
-
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //retrieve the isbn from the request parameter
-  const isbn = req.params.isbn;
-
-  //convert object values to an array and find the matching book
-  const bookList = Object.values(books);
-  const foundBook = bookList.find(book => book.ISBN === isbn);
-
-  //send the book details as response
-  if(foundBook){
-    return res.status(200).json(foundBook);
-  }else{
-    return res.status(401).json({message: "Book not found"});
-  }
-});
  
 
 
